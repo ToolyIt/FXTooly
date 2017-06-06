7224b7e481d56ce862c322d5593eb041bfe8bf50
Ruud Koedooder <ruud@koedooder.net>
2017-06-06 16:18:26 +0100

    check for JavaFX thread

--- src/main/java/nl/fxtooly/ToolyExceptionHandler.java
+++ src/main/java/nl/fxtooly/ToolyExceptionHandler.java
@@ -13,34 +13,38 @@ import javafx.scene.layout.Priority;
 public class ToolyExceptionHandler {
 
 	public static void handle(String message, Exception e){
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("Exception Dialog");
-		alert.setHeaderText(message);
-		alert.setContentText(e.getMessage());
+		if (Thread.currentThread().getName().equals("JavaFX Application Thread")) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Exception Dialog");
+			alert.setHeaderText(message);
+			alert.setContentText(e.getMessage());
 
-		StringWriter sw = new StringWriter();
-		PrintWriter pw = new PrintWriter(sw);
-		e.printStackTrace(pw);
-		String exceptionText = sw.toString();
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			String exceptionText = sw.toString();
 
-		Label label = new Label("Exception stacktrace:");
+			Label label = new Label("Exception stacktrace:");
 
-		TextArea textArea = new TextArea(exceptionText);
-		textArea.setEditable(false);
-		textArea.setWrapText(true);
+			TextArea textArea = new TextArea(exceptionText);
+			textArea.setEditable(false);
+			textArea.setWrapText(true);
 
-		textArea.setMaxWidth(Double.MAX_VALUE);
-		textArea.setMaxHeight(Double.MAX_VALUE);
-		GridPane.setVgrow(textArea, Priority.ALWAYS);
-		GridPane.setHgrow(textArea, Priority.ALWAYS);
+			textArea.setMaxWidth(Double.MAX_VALUE);
+			textArea.setMaxHeight(Double.MAX_VALUE);
+			GridPane.setVgrow(textArea, Priority.ALWAYS);
+			GridPane.setHgrow(textArea, Priority.ALWAYS);
 
-		GridPane expContent = new GridPane();
-		expContent.setMaxWidth(Double.MAX_VALUE);
-		expContent.add(label, 0, 0);
-		expContent.add(textArea, 0, 1);
+			GridPane expContent = new GridPane();
+			expContent.setMaxWidth(Double.MAX_VALUE);
+			expContent.add(label, 0, 0);
+			expContent.add(textArea, 0, 1);
 
-		alert.getDialogPane().setExpandableContent(expContent);
-		alert.showAndWait();
+			alert.getDialogPane().setExpandableContent(expContent);
+			alert.showAndWait();
+		} else {
+			e.printStackTrace();
+		}
 	}
 	public static void handle(Exception e){
 		handle("Error caught!", e);
