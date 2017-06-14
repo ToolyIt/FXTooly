f36a76f82f8420c80f73c96310a392d85fb8a252
Ruud Koedooder <ruud@koedooder.net>
2017-06-14 07:38:03 +0100

    Social tab added

---
+++ src/main/java/it/tooly/fxtooly/tab/social/Social.java
@@ -0,0 +1,9 @@
+package it.tooly.fxtooly.tab.social;
+
+import it.tooly.fxtooly.ToolyPane;
+
+public class Social extends ToolyPane {
+	public Social() {
+		super();
+	}
+}
---
+++ src/main/java/it/tooly/fxtooly/tab/social/SocialController.java
@@ -0,0 +1,11 @@
+package it.tooly.fxtooly.tab.social;
+
+import it.tooly.fxtooly.ToolyPaneController;
+import javafx.fxml.FXML;
+
+public class SocialController implements ToolyPaneController{
+	@FXML
+    public void initialize() {
+
+    }
+}
